09df5f3936eadfdc1fe5f480f431ea7dfd3ba83d
Andrew Calderwood <pandapops3665@gmail.com>
2017-07-27 08:06:15 +0100

    Added a !Stop music command, but it doesnt exit Opus correctly.

--- Util.js
+++ Util.js
@@ -13,16 +13,26 @@ module.exports = {
     ParseMessage: function (message) {
         if (message.content.toLowerCase().startsWith("!help")) {
             message.reply("!gig <HeroName> : Provides wiki page for that Hero");
+            message.reply("!Tom : Plays toms song duhh");
+            message.reply("!StopMusic : stops current sound playing");
         }
 
         if (message.content.startsWith("!Tom")) {
-          this.PlaySoundinChannel(message, "./Sounds/TomsSong.mp3")
+            this.PlaySoundinChannel(message, "./Sounds/TomsSong.mp3")
+        }
+        if (message.content.startsWith("!StopMusic")) {
+            var voiceChannel = message.member.voiceChannel;
+
+            if(voiceChannel){
+                voiceChannel.leave();
+            }
         }
     },
     /// Play a sound in response to a message
     PlaySoundinChannel: function (message, soundFilePath, onExit) {
         var voiceChannel = message.member.voiceChannel;
         voiceChannel.join().then(connection => {
+            
             const dispatcher = connection.playFile(soundFilePath);
             dispatcher.on("end", end => {
                 if (onExit)
